0388b9495a6110e2e0488e0e70b3c8f9d2bc26a2
thatipalliakhila <thatipalliakhila@gmail.com>
2025-03-01 15:20:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
+
+const app=express()
+const PORT=3000
+app.use(express.json());
+
+//Home page api
+app.get('/',(req, res)=>{
+    res.send("<h1 align=center>Welcome to the MERN stack week 2 session</h1>")
+})
+
+//Registration page api
+
+app.post('/register',async(req, res)=>{
+    const {username,email,password}=req.body
+    try{
+        const hashedPassword= await bcrypt.hash(password,10)
+        const user=new User({username,email,password:hashedPassword})
+        await user.save()
+        res.json({message: "User Registred.."})
+        console.log("User Registration completed...")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+//Login page api
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    try{
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) 
+            {
+             return res.status(400).json({ message: "Invalid Credentials" });
+            }
+          res.json({ message: "Login Successful", username: user.username });
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>console.log("DB connected successfully..")
+).catch(
+    (err)=>console.log(err)
+)
+
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    console.log("Server is running on port :"+PORT)
+})
